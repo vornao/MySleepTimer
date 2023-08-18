ddf40b65503287ba97189d547e33ab7d5d8c28df
Luca <migliorluca@gmail.com>
2023-08-18 15:19:57 +0200

    changed theme

--- app/src/main/java/dev/vornao/mysleeptimer/MainActivity.kt
+++ app/src/main/java/dev/vornao/mysleeptimer/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
@@ -21,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // force night mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
         var timerValue = 0
         val addTime = findViewById<Button>(R.id.add_time)
